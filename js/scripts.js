cce19948f5e85855104886dad7fdb6b9b3a83091
ericajlamarche <ericaj.lamarche@gmail.com>
2016-03-20 18:20:45 -0400

    Got jQuery dropdown menu to work. Also working on getting API working and added link for Angular framework.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,26 @@
 
+
+
 /* -----Navigation Dropdown Menu */
-$(document).ready(function() {
+$(document).ready(function(){
     $("#nav-mobile").html($("#nav-main").html());
     $("#nav-trigger span").click(function(){
-        if ($("nav #nav-mobile ul").hasClass("expanded")) {
-            $("nav #nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
+        if ($("nav#nav-mobile ul").hasClass("expanded")) {
+            $("nav#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
             $(this).removeClass("open");
         } else {
-            $("nav #nav-mobile ul").addClass("expanded").slideDown(250);
+            $("nav#nav-mobile ul").addClass("expanded").slideDown(250);
             $(this).addClass("open");
         }
     });
 });
 /*-----------------------------------*/
+
+/*-----Maps API Documentation*/
+function initMap() {
+  var mapDiv = document.getElementById('map');
+  var map = new google.maps.Map(mapDiv, {
+    center: {lat: 44.540, lng: -78.546},
+    zoom: 8
+  });
+}
