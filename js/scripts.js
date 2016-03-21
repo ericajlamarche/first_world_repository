a999ee1f55f93d9b89c785205f5e029a28eb8cb2
ericajlamarche <ericaj.lamarche@gmail.com>
2016-03-20 21:53:19 -0400

    Appended a read more link to columns.

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,51 @@ $(document).ready(function(){
 });
 /*-----------------------------------*/
 
+/*Read More Text Link*/
+
+/*$(function(){ /* to make sure the script runs after page load */
+
+/*    $('a.read_more').click(function(event){ /* find all a.read_more elements and bind the following code to them */
+
+      /*  event.preventDefault(); /* prevent the a from changing the url */
+      /*  $(this).parents('.tertiary').find('.more_text').show(); /* show the .more_text span */
+
+  /*  });
+
+}); */
+
+
+$(function(){ /* to make sure the script runs after page load */
+
+	$('.col').each(function(event){ /* select all divs with the item class */
+
+		var max_length = 150; /* set the max content length before a read more link will be added */
+
+		if($(this).html().length > max_length){ /* check for content length */
+
+			var short_content 	= $(this).html().substr(0,max_length); /* split the content in two parts */
+			var long_content	= $(this).html().substr(max_length);
+
+			$(this).html(short_content+
+						 '<a href="#" class="read_more"><br/>Read More</a>'+
+						 '<span class="more_text" style="display:none;">'+long_content+'</span>'); /* Alter the html to allow the read more functionality */
+
+			$(this).find('a.read_more').click(function(event){ /* find the a.read_more element within the new html and bind the following code to it */
+
+				event.preventDefault(); /* prevent the a from changing the url */
+				$(this).hide(); /* hide the read more button */
+				$(this).parents('.col').find('.more_text').show(); /* show the .more_text span */
+
+			});
+
+		}
+
+	});
+
+
+});
+
+
 /*-----Maps API Documentation*/
 function initMap() {
   var mapDiv = document.getElementById('map');
